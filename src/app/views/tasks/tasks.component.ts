abbef9375ce18875d889befdc595a0bf80946bce
Nikita Tretyakov <qwinikita@gmail.com>
2020-05-19 22:31:46 +0300

    NikitaT
    Переписана таблица списка тасок на Angular Material Table

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/model/task.model';
 import { DataHandlerService } from 'src/app/service/data-handler.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-tasks',
@@ -11,16 +13,38 @@ export class TasksComponent implements OnInit {
 
   public tasks: Task[];
 
+  public displayedColumns: string[] = ['color', 'id', 'title', 'category', 'priority', 'date'];
+  public dataSource: MatTableDataSource<Task>;
+
   constructor(private dataHandler: DataHandlerService) { }
 
   public ngOnInit(): void {
     this.dataHandler.tasksSubject.subscribe(
-      task => this.tasks = task,
+      tasks => this.tasks = tasks,
       err => console.error(err)
     );
+
+    this.dataSource = new MatTableDataSource();
+    this.refreshTable();
   }
 
   public toggleTaskCompleted(task: Task): void {
     task.completed = !task.completed;
+  }
+
+  public getPriorityColor(task: Task): string {
+    if (task.completed) {
+      return '#f8f9fa'
+    }
+
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+
+    return '#fff';
+  }
+
+  private refreshTable() : void {
+    this.dataSource.data = this.tasks;
   }
 }
