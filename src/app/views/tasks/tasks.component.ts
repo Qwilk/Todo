94850dea4a55dfe66e88a79e2d630e686ca561bf
Nikita Tretyakov <qwinikita@gmail.com>
2020-05-20 00:06:41 +0300

    NikitaT
    Добавлена сортировка тасок

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Task } from 'src/app/model/task.model';
 import { DataHandlerService } from 'src/app/service/data-handler.service';
 import { MatTableDataSource } from '@angular/material/table';
-import { ThrowStmt } from '@angular/compiler';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, AfterViewInit {
 
   public tasks: Task[];
 
   public displayedColumns: string[] = ['color', 'id', 'title', 'category', 'priority', 'date'];
   public dataSource: MatTableDataSource<Task>;
+
+  @ViewChild(MatSort, {static: false}) private sort: MatSort;
+  @ViewChild(MatPaginator, {static: false}) private paginator: MatPaginator;
 
   constructor(private dataHandler: DataHandlerService) { }
 
@@ -26,6 +30,10 @@ export class TasksComponent implements OnInit {
 
     this.dataSource = new MatTableDataSource();
     this.refreshTable();
+  }
+
+  public ngAfterViewInit(): void {
+    this.addTableObjects();
   }
 
   public toggleTaskCompleted(task: Task): void {
@@ -46,5 +54,20 @@ export class TasksComponent implements OnInit {
 
   private refreshTable() : void {
     this.dataSource.data = this.tasks;
+    this.addTableObjects();
+
+    this.dataSource.sortingDataAccessor = (task, colName) => {
+      switch(colName) {
+        case 'priority': return task.priority ? task.priority.id : null;
+        case 'category': return task.category ? task.category.title : null;
+        case 'date': return task.date ? task.date.toString() : null; // TODO: исправить сортировку даты
+        case 'title': return task.title;
+      }
+    }
+  }
+
+  private addTableObjects(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 }
